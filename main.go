1f8bcf505ece6ce9e20181c03f66088afdabf55d
Ben Mercer <benmercer76@btinternet.com>
2025-08-29 21:44:57 +0100

    Update it to forward my octoprint server to my url

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/reverseproxy"
 )
 
 func renderWithTime(c *fiber.Ctx, view string, data fiber.Map, layout string) error {
@@ -31,27 +35,37 @@ func main() {
 
 	app.Static("/static", "./static")
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return renderWithTime(c, "index", fiber.Map{"Title": "Home"}, "layout/base")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return renderWithTime(c, "index", fiber.Map{"Title": "Home"}, "layout/base")
+	})
 
-    app.Get("/projects/portfolio", func(c *fiber.Ctx) error {
-        return renderWithTime(c, "projects/portfolio", fiber.Map{"Title": "Portfolio"}, "layout/base")
-    })
+	app.Get("/projects/portfolio", func(c *fiber.Ctx) error {
+		return renderWithTime(c, "projects/portfolio", fiber.Map{"Title": "Portfolio"}, "layout/base")
+	})
 
-    app.Get("/projects/lasertag", func(c *fiber.Ctx) error {
-        return renderWithTime(c, "projects/lasertag", fiber.Map{"Title": "Laser Tag"}, "layout/base")
-    })
+	app.Get("/projects/lasertag", func(c *fiber.Ctx) error {
+		return renderWithTime(c, "projects/lasertag", fiber.Map{"Title": "Laser Tag"}, "layout/base")
+	})
 
-    app.Get("/projects/socialmedia", func(c *fiber.Ctx) error {
-        return renderWithTime(c, "projects/socialmedia", fiber.Map{"Title": "Social Media"}, "layout/base")
-    })
+	app.Get("/projects/socialmedia", func(c *fiber.Ctx) error {
+		return renderWithTime(c, "projects/socialmedia", fiber.Map{"Title": "Social Media"}, "layout/base")
+	})
 
-    app.Get("/projects/software", func(c *fiber.Ctx) error {
-        return renderWithTime(c, "projects/software", fiber.Map{"Title": "Software Development"}, "layout/base")
-    })
+	app.Get("/projects/software", func(c *fiber.Ctx) error {
+		return renderWithTime(c, "projects/software", fiber.Map{"Title": "Software Development"}, "layout/base")
+	})
+
+	octoprintProxy := reverseproxy.NewReverseProxy("192.168.1.87")
+
+	app.All("/octoprint/*", func(c *fiber.Ctx) error {
+		// Strip the "/octoprint" prefix before forwarding
+		newPath := c.OriginalURL()[len("/octoprint"):]
+		c.Path(newPath)
+		octoprintProxy.ServeHTTP(c.Context())
+		return nil
+	})
 
 	fmt.Println("Server starting on http://localhost:3000")
-    
+
 	app.Listen("0.0.0.0:3000")
 }
